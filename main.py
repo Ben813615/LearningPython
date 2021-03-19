0246c3edb3b661b6b780f646a06fd7cabf41b27d
Ben813615 <813615@mystma.org>
2021-03-19 15:27:15 +0000

    madlib

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-print("What is your top 5 movies?\n")
-print("Avengers Endgame")
-print("The Thing")
-print("Deadpool")
-print("Dunkirk")
-print("Joker")
+favFood = "Tacos"
+day = "today"
+print("your favorite food is " + favFood + " " + day)
 
+
+noun = "shoe" 
+food = "bread"
+Room = "bedroom" 
+adjective = "light"
+noun2 = "ball"
+noun3 = "stick"
+adjective2 = "red"
+adverb = "gingerly"
+room2 = "bathroom"
+noun4 = "sink"
+noun 5 = "curtian"
+print("Last week we went to my grandmas house and had " + food + "for supper. " + "After that we all sat in the " + Room + "to open my presents. " + The first gift )
